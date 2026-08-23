b828147020dace004244e0078714579da29413a5
Leonilson <leosouza5555@gmail.com>
2026-08-23 18:05:58 -0300

    config: adiciona output export, compress, poweredByHeader false

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  compress: true,
+  poweredByHeader: false,
+}
 
-export default nextConfig;
+export default nextConfig
